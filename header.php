c2e30fab83d4a45a3b1b704989f830028af3f3fe
Jimmy Pautz <jpautz@dig.ms>
2018-08-31 12:54:19 -0500

    Update header to use flex

--- header.php
+++ header.php
@@ -26,44 +26,42 @@
 				<?php esc_html_e( 'Skip to content', 'gutenbergtheme' ); ?>
 			</a>
 			<header id="masthead" class="site-header">
-
-				<div class="site-branding">
-					<div class="header__bg__orange"></div>
-					<div class="header__bg__gray"></div>
-					<?php
+				<div class="wrapper">
+					<div class="site-branding">
+						<div class="header__bg__orange"></div>
+						<div class="header__bg__gray"></div>
+						<?php
 				the_custom_logo();
 				if ( is_front_page() && is_home() ) : ?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-						<?php else : ?>
-							<p class="site-title">
-								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-									<?php bloginfo( 'name' ); ?>
-								</a>
-							</p>
-							<?php
+							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+							<?php else : ?>
+								<p class="site-title">
+									<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+										<?php bloginfo( 'name' ); ?>
+									</a>
+								</p>
+								<?php
 				endif;
 
 				$description = get_bloginfo( 'description', 'display' );
 				if ( $description || is_customize_preview() ) : ?>
-								<p class="site-description">
-									<?php echo $description; /* WPCS: xss ok. */ ?>
-								</p>
-								<?php
+									<p class="site-description">
+										<?php echo $description; /* WPCS: xss ok. */ ?>
+									</p>
+									<?php
 				endif; ?>
-				</div>
-				<!-- .site-branding -->
+					</div>
+					<!-- .site-branding -->
 
-				<nav id="site-navigation" class="main-navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-						<?php esc_html_e( 'Primary Menu', 'theme' ); ?>
-					</button>
-					<?php
+					<nav id="site-navigation" class="main-navigation">
+						<?php
 					wp_nav_menu( array(
 						'theme_location' => 'menu-1',
 						'menu_id'        => 'primary-menu',
 					) );
 				?>
-				</nav>
-				<!-- #site-navigation -->
+					</nav>
+					<!-- #site-navigation -->
+				</div>
 			</header>
 			<!-- #masthead -->
